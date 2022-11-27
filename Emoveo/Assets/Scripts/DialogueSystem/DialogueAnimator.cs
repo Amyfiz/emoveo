12a033cbcf20ee9c2eb1b4735a58c49b55a0e1c4
Amyfiz <a.solonukhina@gmail.com>
2022-11-27 23:03:18 +0300

    first dialogue updated

---
+++ Emoveo/Assets/Scripts/DialogueSystem/DialogueAnimator.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueAnimator : MonoBehaviour
+{
+    public Animator startAnimation;
+    public DialogueManager dialogueManager;
+
+    public Dialogue dialogue;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        startAnimation.SetBool("IsOpen", true);
+        dialogueManager.StartDialogue(dialogue);
+    }
+    
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        startAnimation.SetBool("IsOpen", false);
+        dialogueManager.EndDialogue();
+    }
+}
