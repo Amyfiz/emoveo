5b4d989b537e23cffce98eab176e865bef2ff5b8
Kreker_ <pbobrovskii@sfedu.ru>
2022-11-28 15:21:13 +0300

    Inventory can be opened and closed now

--- Emoveo/Assets/Scripts/Inventory/Inventory.cs
+++ Emoveo/Assets/Scripts/Inventory/Inventory.cs
@@ -7,4 +7,33 @@ public class Inventory : MonoBehaviour
 {
     public bool[] isFull;
     public GameObject[] slots;
+    public GameObject inventory;
+    private bool inventoryOn;
+
+    private void Start()
+    {
+        inventoryOn = false;
+    }
+
+    public void ShowInventory()
+    {
+        if (inventoryOn == false)
+        {
+            inventoryOn = true;
+            inventory.SetActive(true);
+        }
+        else if (inventoryOn == true)
+        {
+            inventoryOn = false;
+            inventory.SetActive(false);
+        }
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            ShowInventory();
+        }
+    }
 }
