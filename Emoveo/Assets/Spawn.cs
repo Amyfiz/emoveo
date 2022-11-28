7d1943c7a3f207ea7223624770a0ed92e07f9cb7
Kreker_ <pbobrovskii@sfedu.ru>
2022-11-28 15:10:37 +0300

    player can drop items

--- Emoveo/Assets/Spawn.cs
+++ Emoveo/Assets/Spawn.cs
@@ -14,7 +14,7 @@ public class Spawn : MonoBehaviour
 
     public void SpawnDroppedItem()
     {
-        Vector2 playerPos = new Vector2(player.position.x + 2, player.position.y - 1);
+        Vector2 playerPos = new Vector2(player.position.x + 5, player.position.y - 2);
         Instantiate(item, playerPos, Quaternion.identity);
     }
 }
