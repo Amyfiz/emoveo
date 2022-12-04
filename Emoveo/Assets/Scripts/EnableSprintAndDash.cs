14177dc01ae3d0ca203ad3c0990d333fa70cd5a0
Amyfiz <a.solonukhina@gmail.com>
2022-12-04 13:07:49 +0300

    enable sprint and dash script added

---
+++ Emoveo/Assets/Scripts/EnableSprintAndDash.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableSprintAndDash : MonoBehaviour
+{
+    public PlayerController playerController;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        playerController.abilityToDash = true;
+        playerController.abilityToSprint = true;
+    }
+}
