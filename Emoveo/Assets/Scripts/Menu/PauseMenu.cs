5b9fefd63aaf704891529177160bc349cd961ba7
Amyfiz <a.solonukhina@gmail.com>
2023-02-08 12:53:40 +0300

    Pause Menu script edited

--- Emoveo/Assets/Scripts/Menu/PauseMenu.cs
+++ Emoveo/Assets/Scripts/Menu/PauseMenu.cs
@@ -5,6 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
+    private bool pauseMenuOn;
+    public GameObject pauseMenu;
+    private Player player;
+    
+    private void Start()
+    {
+        pauseMenuOn = false;
+        pauseMenu.SetActive(false);
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ShowPauseMenu();
+            player.abilityToDash = false;
+            player.abilityToMove = false;
+            player.abilityToSprint = false;
+        }
+    }
+    
+    private void ShowPauseMenu()
+    {
+        if (!pauseMenuOn)
+        {
+            pauseMenu.SetActive(!pauseMenu.activeSelf);
+        }
+    }
+
     public void QuitToMainMenu()
     {
         SceneManager.LoadScene("1_StartMenu");
