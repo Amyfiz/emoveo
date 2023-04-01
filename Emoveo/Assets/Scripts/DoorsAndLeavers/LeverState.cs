0a197b81c76cca3f8321516fc05dfc6bfdb13122
Amyfiz <a.solonukhina@gmail.com>
2023-04-01 21:25:05 +0300

    JustInCaseYouMessUp + DeathCrystals scripts added

--- Emoveo/Assets/Scripts/DoorsAndLeavers/LeverState.cs
+++ Emoveo/Assets/Scripts/DoorsAndLeavers/LeverState.cs
@@ -17,7 +17,6 @@ public class LeverState : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.F))
             {
                 activated = !activated;
-                
             }
         }
     }
