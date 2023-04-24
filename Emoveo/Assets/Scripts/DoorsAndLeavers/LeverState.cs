f6bdae671c098b53c2c4937b892b602b15f9823c
KreKer_ <platon.bob@gmail.com>
2023-04-24 17:57:59 +0300

    lever fix
    levers work perfectly, just like me C^:

--- Emoveo/Assets/Scripts/DoorsAndLeavers/LeverState.cs
+++ Emoveo/Assets/Scripts/DoorsAndLeavers/LeverState.cs
@@ -1,14 +1,22 @@
+using System;
 using UnityEngine;
 
 public class LeverState : MonoBehaviour
 {
     public GameObject lever;
     protected internal bool activated = false;
+    public Collider2D collider;
 
-    public void OnTriggerStay2D(Collider2D other)
+    private void Awake()
     {
-        if (other.CompareTag("Player"))
+        collider = gameObject.GetComponent<Collider2D>();
+    }
+
+    public void Update()
+    {
+        if (collider.IsTouching(GameObject.FindGameObjectWithTag("Player").GetComponent<Collider2D>()))
         {
+            Debug.Log("ЕСТЬ КОНТАКТ");
             if (Input.GetKeyDown(KeyCode.F))
             {
                 activated = true;
